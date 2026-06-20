5892600b61d610a27022d8a9cec8ee2524de6c4f
Sagar Nath <nathsagar96@gmail.com>
2026-06-21 01:00:28 +0530

    feat: integrate OpenAPI annotations for DTOs, responses, and controllers; add example values for improved API documentation and enable Swagger UI

---
+++ src/main/java/com/morsel/config/OpenApiConfig.java
@@ -0,0 +1,59 @@
+package com.morsel.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class OpenApiConfig {
+
+    private final BuildProperties buildProperties;
+
+    public OpenApiConfig(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8080");
+        server.setDescription("Development server");
+
+        Contact contact =
+                new Contact().name("Sagar Nath").email("nathsagar96@gmail.com").url("https://github.com/nathsagar96");
+
+        License license = new License().name("MIT License").url("https://opensource.org/licenses/MIT");
+
+        Info info = new Info()
+                .title("Morsel API")
+                .description("Recipe sharing platform built with Spring Boot 4.1.0")
+                .version(buildProperties.getVersion())
+                .contact(contact)
+                .license(license);
+
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .description("Enter your JWT token");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("Bearer");
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(server))
+                .addSecurityItem(securityRequirement)
+                .components(new Components().addSecuritySchemes("Bearer", bearerAuth));
+    }
+}
